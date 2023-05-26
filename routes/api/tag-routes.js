85d1981b7f51ae53dee01b67e059381041489b39
Karun Thiyagarajah <tknanthan@gmail.com>
2023-05-26 19:31:14 +1000

    completed code is uploaded

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -32,12 +32,38 @@ router.get('/:id', async (req, res) => {
   }  
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const { id } = req.params;
+    const { tag_name } = req.body;
+
+    // Update the tag_name
+    const tagData = await Tag.update(
+      { tag_name },
+      {
+        where: { id }
+      }
+    );
+
+    if (tagData[0] === 0) {
+      return res.status(404).json({ error: 'No tag found with this id!' });
+    }
+
+    res.status(200).json({ message: 'Tag updated successfully' });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
